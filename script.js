a90322a89dcee133abb1028d1eba95ce4e574068
TeSakax <g.merkviladze98@gmail.com>
2023-11-10 17:16:20 +0400

    now for sure :D

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const birthGuga = 1998;
 let currentYear = 2023;
-const ageGuga = currentYear - birthGuga;
+var ageGuga = currentYear - birthGuga;
 console.log(`Guga will be ${(currentYear - birthGuga) + 2 } years old in 2 years.`);
 
 const Guga = {
     first: "Guga",
     last: "Merkviladze",
-    age: 25,
+    age: ageGuga,
 };
-console.log(`His name is ${Guga.first} ${Guga.last}, age of ${Guga.age} `)
+console.log(`His name is ${Guga.first} ${Guga.last}. 
+Borin in ${birthGuga}, so he is ${Guga.age} years old. `);
 
 let rectangleLength = 7;
 let rectangleWidth = 5;
 const rectangleArea = rectangleLength * rectangleWidth;
-console.log(`Rectangle area is ${rectangleArea} `)
+console.log(`Rectangle area is ${rectangleArea} `);
